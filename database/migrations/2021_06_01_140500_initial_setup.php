0d598efccdc3f952defe9d877b0a4348db0b694f
Kanhaiya <kanhaiya_murarka2001@yahoo.com>
2021-06-01 20:22:54 +0545

    added inital setup migrations

---
+++ database/migrations/2021_06_01_140500_initial_setup.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class InitialSetup extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('link_master', function (Blueprint $table) {
+            $table->increments('id');
+            $table->mediumText('link');
+            $table->char('hash',16)->unique();
+            $table->dateTime('created_at');
+            $table->timestamp('updated_at');
+        });
+        DB::update("ALTER TABLE link_master AUTO_INCREMENT = 1;");
+
+        Schema::create('stats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('hash',16);
+            $table->integer('click_count')->default(1);
+            $table->ipAddress('ip');
+            $table->text('ua');
+            $table->dateTime('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
